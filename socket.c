6cd245f547d9321fcd3e939df2a59dfaed3c6a05
Arthur Diniz <arthurbdiniz@gmail.com>
2019-04-11 09:21:51 -0300

    Send our Message to the Server
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- socket.c
+++ socket.c
@@ -8,8 +8,7 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netdb.h>
-
-// #define PORT     8080
+#include <time.h>   
 
 typedef struct {
 
@@ -47,24 +46,24 @@ void error(const char *error) {
 
 int main(int argc, char ** arg) {
 
-	// Create and zero out the packet. All 48 bytes worth.
+	printf("Set the 48-byte string");
 
+	// Create and zero out the packet. All 48 bytes worth.
 	ntp_packet packet = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 	memset( &packet, 0, sizeof( ntp_packet ) );
 
-	// Set the first byte's bits to 00,011,011 for li = 0, vn = 3, and mode = 3. The rest will be left set to zero.
-
+	// Set the first byte's bits to 00,011,011 for li = 0, vn = 3, and mode = 3. The rest will be left set to zero.s
 	*( ( char * ) &packet + 0 ) = 0x1b; // Represents 27 in base 10 or 00011011 in base 2.
 
+	printf("Setup our Socket and Server Data Structure");
 
 	// Create a UDP socket, convert the host-name to an IP address, set the port number,
 	// connect to the server, send the packet, and then read in the return packet.
-
 	struct sockaddr_in serv_addr; // Server address data structure.
 	struct hostent *server;      // Server data structure.
 	int sockfd;
-	char host_name[] = "google.com"; 
+	char host_name[] = "time-nw.nist.gov"; 
 	int port = 8080;
 
 	sockfd = socket( AF_INET, SOCK_DGRAM, IPPROTO_UDP ); // Create a UDP socket.
@@ -78,18 +77,50 @@ int main(int argc, char ** arg) {
 	error( "ERROR, no such host" );
 
 	// Zero out the server address structure.
-
 	bzero( ( char* ) &serv_addr, sizeof( serv_addr ) );
 
 	serv_addr.sin_family = AF_INET;
 
 	// Copy the server's IP address to the server address structure.
-
 	bcopy( ( char* )server->h_addr, ( char* ) &serv_addr.sin_addr.s_addr, server->h_length );
 
 	// Convert the port number integer to network big-endian style and save it to the server address structure.
-
 	serv_addr.sin_port = htons( port );
+
+	printf("Send our Message to the Server");
+
+	// Call up the server using its IP address and port number.
+	if ( connect( sockfd, ( struct sockaddr * ) &serv_addr, sizeof( serv_addr) ) < 0 )
+	error( "ERROR connecting" );
+
+	// Send it the NTP packet it wants. If n == -1, it failed.
+	int n = write( sockfd, ( char* ) &packet, sizeof( ntp_packet ) );
+
+	if ( n < 0 )
+	error( "ERROR writing to socket" );
+
+	// Wait and receive the packet back from the server. If n == -1, it failed.
+	n = read( sockfd, ( char* ) &packet, sizeof( ntp_packet ) );
+
+	if ( n < 0 )
+	error( "ERROR reading from socket" );
+
+	// These two fields contain the time-stamp seconds as the packet left the NTP server.
+	// The number of seconds correspond to the seconds passed since 1900.
+	// ntohl() converts the bit/byte order from the network's to host's "endianness".
+
+	packet.txTm_s = ntohl( packet.txTm_s ); // Time-stamp seconds.
+	packet.txTm_f = ntohl( packet.txTm_f ); // Time-stamp fraction of a second.
+
+	// Extract the 32 bits that represent the time-stamp seconds (since NTP epoch) from when the packet left the server.
+	// Subtract 70 years worth of seconds from the seconds since 1900.
+	// This leaves the seconds since the UNIX epoch of 1970.
+	// (1900)------------------(1970)**************************************(Time Packet Left the Server)
+
+	time_t txTm = ( time_t ) ( packet.txTm_s - 2208988800U );
+
+	// Print the time we got from the server, accounting for local timezone and conversion from UTC time.
+	printf( "Time: %s", ctime( ( const time_t* ) &txTm ) );
 
 	return 0;
 }
